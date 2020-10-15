9970124dddefa26aa697d8cd779e095f8a7601a3
NsuarKnights <Nicholas.Suarez001@knights.ucf.edu>
2020-10-15 17:47:42 -0400

    Progress on Assignment 1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,9 +4,60 @@ namespace Zork
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                /*switch (command)
+                {
+                
+                    case "QUIT":
+                        command = Commands.QUIT;
+                        break;
+
+                    case "LOOK":
+                        command = Commands.LOOK;
+                        break;
+
+                    case "NORTH":
+                        command = Commands.NORTH;
+                        break;
+
+                    case "SOUTH":
+                        command = Commands.SOUTH;
+                        break;
+
+                    case "EAST":
+                        command = Commands.EAST;
+                        break;
+
+                    case "WEST":
+                        command = Commands.WEST;
+                        break;
+
+                    default:
+                        command = Commands.UNKNOWN;
+                        break;
+                
+
+                };*/
+
+
+            }
+
+           
         }
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
+        
+
     }
 }
