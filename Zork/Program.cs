7c8229dbe52aa54d013ef89aded8963147df8afc
NsuarKnights <Nicholas.Suarez001@knights.ucf.edu>
2020-11-28 22:06:35 -0500

    ZORK PART 4 COMMIT
    This is the Submission state for Assignment 7 - Zork - Part 4.
    
    This was one hell of an assignment to tackle because I couldn't wrap my head a round of a lot of things in it, but with help from classmates I'm pretty sure I understand what's going on in the code. Me from 2 years ago would be proud.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,165 +8,21 @@ using Newtonsoft.Json;
 
 namespace Zork
 {
-    internal class Program
+    class Program
     {
-
-        public static Room CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column];
-            }
-        }
-
-        private enum CommandLineArguments
-        {
-            RoomsFilename = 0
-        }
-
         static void Main(string[] args)
         {
-            const string defaultRoomsFilename = "Rooms.txt";
-            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
 
+            Game game = Game.Load(gameFilename);
             Console.WriteLine("Welcome to Zork!");
-           // string roomsFilename = "Rooms.txt";
-            InitilaizeRoomDescriptions(roomsFilename);
-
-            Commands command = Commands.UNKNOWN;
-            while (command != Commands.QUIT)
-            {
-                Room previousRoom = null;
-                Console.WriteLine(CurrentRoom);
-                if (previousRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                }
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                switch (command)
-                {
-
-
-
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
-                        break;
-
-                    case Commands.LOOK:
-                        Console.WriteLine(CurrentRoom.Description);
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command) == false)
-                        {
-                            Console.WriteLine("The way is shut!");
-                        }
-                        break;
-                    
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-
-
-                }
-            }  
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
-        private static readonly Dictionary<string, Room> roomMap;
-        static Program()
+        private enum CommandLineArguments
         {
-            roomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-            {
-                roomMap[room.Name] = room;
-            }
+            GameFilename = 0
         }
-
-        private enum Fields
-        {
-            Name = 0,
-            Description
-        }
-
-        private static void InitilaizeRoomDescriptions(string roomsFilename)
-        {
-            const string fieldDelimiter = "##";
-            const int expectedFieldCount = 2;
-
-            string[] lines = File.ReadAllLines(roomsFilename);
-            foreach (string line in lines)
-            {
-                string[] fields = line.Split(fieldDelimiter);
-                if (fields.Length != expectedFieldCount)
-                {
-                    throw new InvalidDataException("Invalid record.");
-                }
-
-                string name = fields[(int)Fields.Name];
-                string description = fields[(int)Fields.Description];
-
-                roomMap[name].Description = description;
-            }
-        }
-
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(IsDirection(command), "Invalid direction.");
-
-            bool isValidMove = true;
-            switch (command)
-            {
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row++;
-                    break;
-
-                case Commands.SOUTH when Location.Row > 0:
-                    Location.Row--;
-                    break;
-
-                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Location.Column++;
-                    break;
-
-                case Commands.WEST when Location.Column > 0:
-                    Location.Column--;
-                    break;
-
-                default:
-                    isValidMove = false;
-                    break;
-            }
-
-            return isValidMove;
-        }
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-        private static readonly Room[,] Rooms =
-        {
-            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-        };
-
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
-
-        private static (int Row, int Column) Location = (1, 1);
-
-        
-
     }
 }
